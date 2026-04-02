b757256bf7491eaa637e6ec0a03f5e930ab3ba02
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-04-02 18:16:08 +0100

    First commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title> {{ $title }} </title>
+</head>
+
+<body class="my-4 grid justify-center bg-purple-950 text-white">
+    
+
+
+    <div class="mb-4">
+        <ul class="border-2 flex py-2 justify-center">
+
+            <li class="px-2"><a class="hover:font-bold" href="/">Homepage</a></li>
+            <li class="px-2"><a class="hover:font-bold" href="/login">Login</a></li>
+            <li class="px-2"><a class="hover:font-bold" href="/register">Register</a></li>
+
+
+            @if(auth()->user())
+                <li class="px-2"><a class="hover:font-bold" href="/addnotes">Add Notes</a></li>
+                <li class="px-2"><a class="hover:font-bold" href="/notes">View Notes</a></li>
+                <li class="px-2"><a class="hover:font-bold" href="/editnotes">Edit Notes</a></li>
+                <li class="px-2"><a class="hover:font-bold" href="/logout">Logout</a></li>
+            @endif
+
+            
+
+
+        </ul>
+    </div>
+    
+
+    <div class="border-2 p-4">
+        {{ $slot }}
+    </div>
+    
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+
+
+    <x-slot:title>
+        Homepage
+    </x-slot>
+
+
+    <h1 class="font-bold text-4xl">This is a Homepage</h1>
+
+
+    <div class="mt-4">
+        <p>This is the homepage for the Note App that uses a Database Relationship</p>
+    </div>
+
+
+</x-layout>
