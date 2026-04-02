1cd722a4d109599605f6b6ca5713d6945b6b37db
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-04-02 20:44:03 +0100

    Opening login and registration page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
+
 Route::get('/', function () {
     return view('welcome');
 })->middleware('guest');
