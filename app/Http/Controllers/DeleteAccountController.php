3fe85579448c079ccab97e357d4c7f643d46cb77
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-04-18 22:21:21 +0100

    Adding delete account function (which also deletes related notes)

--- app/Http/Controllers/DeleteAccountController.php
+++ app/Http/Controllers/DeleteAccountController.php
@@ -4,10 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
+
 class DeleteAccountController extends Controller
 {
     public function OpenDeleteAccountPage()
     {
         return view('pages.deleteaccount');
+    }
+
+
+    public function DeleteAccount($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect('/login')
+        ->with('AccountDeletedSuccess', 'Account succesfully deleted');
     }
 }
